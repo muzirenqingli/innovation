911aa36da81873f06437fb1f74ac75e4498da401
muzirenqingli <li102qian@163.com>
2019-04-12 18:01:17 +0800

    20190412 submit

--- src/compontent/App.vue
+++ src/compontent/App.vue
@@ -1,16 +1,21 @@
 
 
+
 <template>
- <div>123</div>
+    <div><headercompontent></headercompontent></div>
+   
 </template>
 
 <script>
+import headercompontent from './headerCompontent.vue'
 export default {
-    
+    components:{
+        headercompontent
+    }
 }
 </script>
 
-<style lang="">
+<style lang="less">
     
 </style>
 
---
+++ src/compontent/headercompontent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="topbar">
+        <img src="../images/top-logo.png" class="logo" />
+        <div class="serch-wrap">
+            <input type="text" class="serch-text" />
+            <button>搜索</button>
+        </div>
+        <img src="../images/header-faxian.png" class="findicon"/>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scope>
+    .topbar{ position: fixed; left: 0px; top: 0px; display: inline-block;}
+    .logo{ width:25% }
+    .serch-wrap{ display: inline-block}
+    .findicon{ width: 7%}
+</style>
+
